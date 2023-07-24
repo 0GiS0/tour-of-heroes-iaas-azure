7ab175f215f30dcfb8478ce05538d25d7a2378ae
0GiS0 <giselatb@outlook.com>
2023-07-24 11:22:04 +0200

    Configure IIS in front end vm

--- 00-variables.sh
+++ 00-variables.sh
@@ -33,8 +33,9 @@ API_VM_NSG_NAME="api-vm-nsg"
 
 # Frontend VM on Azure
 FRONTEND_VM_NAME="frontend-vm"
-FRONTEND_VM_IMAGE="Ubuntu2204"
+FRONTEND_VM_IMAGE="MicrosoftWindowsServer:WindowsServer:2022-Datacenter:latest"
 FRONTEND_VM_ADMIN_USERNAME="frontendadmin"
 FRONTEND_VM_ADMIN_PASSWORD="fr0nt#nd@dmin123"
+FRONTEND_VM_NSG_NAME="frontend-vm-nsg"
 
 echo -e "Variables loaded from 00-variables.sh"
--- 04-create-front-end-vm.sh
+++ 04-create-front-end-vm.sh
@@ -1,6 +1,6 @@
 echo -e "Create a frontend vm named $FRONTEND_VM_NAME with image $FRONTEND_VM_IMAGE"
 
-az vm create \
+FQDN_FRONTEND_VM=$(az vm create \
 --resource-group $RESOURCE_GROUP \
 --name $FRONTEND_VM_NAME \
 --image $FRONTEND_VM_IMAGE \
@@ -8,8 +8,36 @@ az vm create \
 --admin-password $FRONTEND_VM_ADMIN_PASSWORD \
 --vnet-name $VNET_NAME \
 --subnet $FRONTEND_SUBNET_NAME \
---public-ip-address "" \
---nsg "" \
---size $VM_SIZE
+--public-ip-address-dns-name tour-of-heroes-frontend-vm \
+--nsg $FRONTEND_VM_NSG_NAME \
+--size $VM_SIZE --query "fqdns" -o tsv)
 
-echo -e "Frontend vm created"
+echo -e "Frontend vm created with FQDN $FQDN_FRONTEND_VM"
+
+echo -e "Create a network security group rule for ssh port 22 for PSSession"
+az network nsg rule create \
+--resource-group $RESOURCE_GROUP \
+--nsg-name $FRONTEND_VM_NSG_NAME \
+--name AllowSsh \
+--priority 1001 \
+--destination-port-ranges 22 \
+--direction Inbound
+
+echo -e "Execute PowerShell"
+pwsh
+
+Write-Output "Install IIS on the frontend vm"
+Invoke-AzVMRunCommand -Name $FRONTEND_VM_NAME -ResourceGroupName $RESOURCE_GROUP -CommandId 'RunPowerShellScript' -ScriptString 'Install-WindowsFeature -name Web-Server -IncludeManagementTools'
+
+Write-Output "Download URL Rewrite Module"
+Invoke-AzVMRunCommand -Name $FRONTEND_VM_NAME -ResourceGroupName $RESOURCE_GROUP -CommandId 'RunPowerShellScript' -ScriptString 'Invoke-WebRequest -Uri "https://www.microsoft.com/web/handlers/webpi.ashx?command=getinstallerredirect&appid=urlrewrite2" -OutFile C:\Temp\urlrewrite2.exe'
+
+Write-Output "Install URL Rewrite Module"
+Invoke-AzVMRunCommand -Name $FRONTEND_VM_NAME -ResourceGroupName $RESOURCE_GROUP -CommandId 'RunPowerShellScript' -ScriptString 'C:\Temp\urlrewrite2.exe /install /quiet'
+
+Write-Output "Create a folder for the frontend app"
+Invoke-AzVMRunCommand -Name $FRONTEND_VM_NAME -ResourceGroupName $RESOURCE_GROUP -CommandId 'RunPowerShellScript' -ScriptString 'mkdir C:\inetpub\wwwroot\frontend'
+
+Write-Output "Download the last release of the frontend app from github"
+Invoke-AzVMRunCommand -Name $FRONTEND_VM_NAME -ResourceGroupName $RESOURCE_GROUP -CommandId 'RunPowerShellScript' -ScriptString 'Invoke-WebRequest -Uri "
+
